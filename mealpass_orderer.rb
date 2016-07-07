9cfa168f51978e63785b33674317ce2759d8e95e
Petr Gazarov <petrgazarov@gmail.com>
2016-07-06 23:46:58 -0400

    login and enter address

--- mealpass_orderer.rb
+++ mealpass_orderer.rb
@@ -2,21 +2,78 @@ require 'rubygems'
 require 'selenium-webdriver'
 require 'pry'
 
-driver = Selenium::WebDriver.for :chrome
-wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+class MealpassOrderer
+  def self.run
+    new.run
+  end
 
-driver.get "https://mealpass.com/login"
-wait
+  def initialize
+    @driver = Selenium::WebDriver.for :chrome
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+  end
 
-wait.until { driver.find_element(:name, "email").displayed? }
+  def run
+    login
 
-driver.find_element(:name, "email").send_keys("petrgazarov@gmail.com")
-driver.find_element(:name, "password").send_keys("n7T1351#2%f@")
+    (driver.close; return) if meal_reserved?
 
-driver.find_element(:link_text, 'Log in').click
-driver.find_element(:xpath,"//input[@value='Log in']").click
+    enter_address
 
-driver.find_element(:xpath,"//input[@value='SUBMIT']").click
-driver.find_element(:xpath,"//input[@value='PICKUP TIME']").click
-driver.find_element(:xpath,"//input[@value='RESERVE NOW']").click
 
+    num_choices = driver.find_elements(:css, '.meal').length
+    pick_number = (0...num_choices).to_a.sample
+
+    driver.find_elements(:css, '.meal')[pick_number].click
+
+    wait.until do
+      driver
+        .find_elements(:css, '.meal')[pick_number]
+        .find_element(:xpath, "//span[contains(., 'PICKUP TIME')]").displayed?
+    end
+
+    driver
+      .find_elements(:css, '.meal')[pick_number]
+      .find_element(:xpath, "//span[text()='PICKUP TIME']").click
+
+    driver.find_element(:xpath, "//span[text()='PICKUP TIME']").click
+
+    # driver.find_element(:xpath,"//input[@value='SUBMIT']").click
+    # driver.find_element(:xpath,"//input[@value='PICKUP TIME']").click
+    # driver.find_element(:xpath,"//input[@value='RESERVE NOW']").click
+
+    driver.close
+  end
+
+  attr_reader :driver, :wait
+
+  private
+
+  def login
+    driver.get "https://mealpass.com/login"
+    wait.until { driver.find_element(:name, "email").displayed? }
+
+    driver.find_element(:name, "email").send_keys("petrgazarov@gmail.com")
+    driver.find_element(:name, "password").send_keys("n7T1351#2%f@")
+
+    driver.find_element(:xpath, '//button[contains(., "Log in")]').click
+
+    wait.until { driver.title.downcase == 'mealpass | portal' }
+  end
+
+  def meal_reserved?
+    driver.page_source.include? 'Your meal is reserved'
+  end
+
+  def enter_address
+    wait.until { driver.find_element(:xpath, "//input[@placeholder='Search Location ...']").displayed? }
+    driver
+      .find_element(:xpath, "//input[@placeholder='Search Location ...']")
+      .send_keys('22 West 19th Street, New York, NY, United States')
+
+    driver.action.send_keys(:enter).perform
+
+    wait.until { driver.find_element(:css, '.lazy-loading').displayed? }
+  end
+end
+
+MealpassOrderer.run
