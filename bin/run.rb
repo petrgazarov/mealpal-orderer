58ba40fee15b0d87def26042283beb040944fb1c
Petr Gazarov <petrgazarov@gmail.com>
2016-07-10 18:59:18 -0400

    Remove pry

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,4 @@
 require_relative '../lib/mealpass_orderer'
-require 'pry'
 
 CURRENT_DIR = File.expand_path(File.dirname(__FILE__))
 CHROMEDRIVER_FN = File.join(File.absolute_path('..', CURRENT_DIR), "bin/chromedriver")
